9967dd91b77d0c204714b62e44dfb40799e04f14
Lisa Hammarstrand <lisa@Lisas-Air.lan>
2020-04-03 16:47:09 +0200

    Fixed the styling of the back card

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -18,37 +18,40 @@ export const Card = ({
   const [clicked, setClicked] = useState(false)
 
   // If the card is clicked render this
-  if (clicked)
-
+  if (clicked) {
+    // Note className "card clicked-card" to tie the cards together
     return (
-      <article className="clicked-card">
-        <header className="top-section" style={{ background: primary }}>
+      <article className="card clicked-card">
+        <section className="top-section" style={{ background: primary }}>
           <h1 style={{ color: headingColor }}>{heading}</h1>
-        </header>
+        </section>
         <section className="light-part" style={{ background: secondary }}>
           <span role="img" aria-label="Emoji" className="emoji">{emoji}</span>
         </section>
       </article>
     )
 
-  // If card is not clicked render this
-  return (
-    <article className="card">
-      <section className="top-section" style={{ background: primary }}>
-        <h1 style={{ color: headingColor }}>{heading}</h1>
-        <span role="img" aria-label="Emoji" className="emoji">
-          {emoji}
-        </span>
-      </section>
-      <section className="light-part" style={{ background: secondary }}>
-        <h2 style={{ color: subtitleColor }}>{subtitle}</h2>
-        <button
-          style={{ background: buttonColor, color: buttonTextColor }}
-          onClick={() => setClicked(!clicked)}>
-          {buttonText}
-        </button>
-      </section>
-    </article>
-  )
+  } else {
+    // If card is not clicked render this
+    return (
+      <article className="card">
+        <section className="top-section" style={{ background: primary }}>
+          <h1 style={{ color: headingColor }}>{heading}</h1>
+          <span role="img" aria-label="Emoji" className="emoji">
+            {emoji}
+          </span>
+        </section>
+        <section className="light-part" style={{ background: secondary }}>
+          <h2 style={{ color: subtitleColor }}>{subtitle}</h2>
+          <button
+            type="button"
+            style={{ background: buttonColor, color: buttonTextColor }}
+            onClick={() => setClicked(!clicked)}>
+            {buttonText}
+          </button>
+        </section>
+      </article>
+    )
+  }
 }
 
